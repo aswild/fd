6cf67c3068c28c10165dce6d3237fdbecc02a8eb
Allen Wild <allenwild93@gmail.com>
2020-07-30 12:24:19 -0400

    zsh completions: fix bug where only one path gets completed
    Running `fd pattern path1 p<TAB>` doesn't try to autocomplete path2
    because clap generates the completions wrong.
    
    Rather than '*::path -- help text', clap generates '::path - help
    text', which only completes once.
    
    Fix with the lovely hack of `sed -i "s/'::path/'*::path/" _fd`.

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
+use std::ffi::OsString;
 use std::fs;
+use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
 use clap::Shell;
@@ -20,7 +22,9 @@ fn main() {
     // determine version from "git describe", unless disabled
     if !cfg!(feature = "fd-no-gitversion") {
         let git_describe_out = Command::new("git")
-            .arg("describe").arg("--tags").arg("--dirty=+")
+            .arg("describe")
+            .arg("--tags")
+            .arg("--dirty=+")
             .stderr(Stdio::inherit())
             .output();
         if let Ok(out) = git_describe_out {
@@ -45,4 +49,25 @@ fn main() {
     app.gen_completions("fd", Shell::Fish, &outdir);
     app.gen_completions("fd", Shell::Zsh, &outdir);
     app.gen_completions("fd", Shell::PowerShell, &outdir);
+
+    // HACK! As usual, clap is incapable of generating correct zsh completions.
+    // In this case, the path option gets set as '::path -- ...' in _arguments,
+    // which only takes one path rather than multiple, so only one file path gets
+    // completed rather than all of them.
+    // Run this through sed (because I'm too lazy to do a whole regex filter
+    // thing here) to change ::path into *::path to fix this.
+    let zshcomp_path: PathBuf = [&outdir, &OsString::from("_fd")].iter().collect();
+    if zshcomp_path.is_file() {
+        let sed_status = Command::new("sed")
+            .arg("-i")
+            .arg("s/'::path/'*::path/")
+            .arg("--")
+            .arg(zshcomp_path)
+            .status();
+        match sed_status {
+            Ok(s) if s.success() => (),
+            Ok(_) => eprintln!("zsh completion sed fixup failed"),
+            Err(e) => eprintln!("failed to spawn zsh completion sed fixup: {}", e),
+        };
+    }
 }
